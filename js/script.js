38bb7855de986d65ee17d6c2c4c62e609b037863
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2022-12-08 21:25:06 -0300

    botei pra la. criação dos videos de fundo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 
 
+videos = ['videos/video2.mp4', 'videos/video3.mp4', 'videos/video4.mp4', 'videos/video5.mp4','videos/video6.mp4', 'videos/video1.mp4']
+list = []
+clicks = 0
 function changeVideo(){
-    document. getElementById('url').src = "videos/video2.mp4"
+    document. getElementById('url').src = `${videos[clicks]}`
+    if(clicks == 5){
+        clicks = 0
+    }
+    clicks++
 }
+
