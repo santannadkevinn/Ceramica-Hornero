f6f3ec531e0337a508cd858e6180894914db4538
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2022-12-09 12:54:39 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -9,5 +9,18 @@ function changeVideo(){
         clicks = 0
     }
     clicks++
+    let logo = document.getElementById('logo')
+    let logoImg = ['imagens/logo-click-aqui.png', 'imagens/redonda-logo-png.png']
+    logo.src = logoImg[0]
+}
+
+
+//change logo
+var currentTime = new Date().getMinutes();
+
+if (44 <= currentTime && currentTime < 45){
+    
+}else if (46 <= currentTime && currentTime < 47){
+    logo.src = logoImg[1]
 }
 
