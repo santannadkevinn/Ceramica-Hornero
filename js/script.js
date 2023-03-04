ef4a507ffc0dd874bbddb4bbbcc71d55d70c9c57
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2023-03-04 16:02:22 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/*onclick="changeVideo()
 
 videos = ['videos/video2.mp4', 'videos/video3.mp4', 'videos/video4.mp4', 'videos/video5.mp4','videos/video6.mp4', 'videos/video1.mp4']
 list = []
@@ -9,9 +9,26 @@ function changeVideo(){
         clicks = 0
         
     }
-    if(clicks == 5){
-         document.getElementById('logo').src = 'imagens/redonda-logo-png.png' 
-    }
     clicks++
 }
+*/
+count = 0
+function changeAll() {
+    let article = document.querySelector('section')
+    if(count % 2 == 0){
+        document. getElementById('url').style.display = 'none'
+        const body = document.querySelector('body')
+        body.style.backgroundColor = 'white'
+        article.style.display = 'block'
+    }
+    if(count % 2 == 1){
+        document. getElementById('url').style.display = 'block'
+        article.style.display = 'none'
+        
+    }
+    count++
+}
+
+const change = document.querySelector('[change-all]')
+change.onclick = changeAll
 
